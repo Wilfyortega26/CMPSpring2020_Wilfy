9e7d0a99b66ef72a9d313bea68c8aaba03e97709
Wilfyortega26 <60441221+Wilfyortega26@users.noreply.github.com>
2020-01-29 11:55:02 -0500

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Wilfy
+ * Created    :1.29.20
+ * description: This program calculates the Birth year
+ *
+ **/
+import java.util.Scanner;
+public class FirstProgram {
+	 //This is the first method the JVM looks for, to run
+	 public static void main(String[] args) {
+		 Scanner keyboard = new Scanner(System.in); 
+	  System.out.println("Welcome to my first"
+				 +"Program \nthis program calculates"
+				 +"the birth year");
+		 System.out.print("Enter your age: ");
+		 int age       = keyboard.nextInt();
+		 int birthYear = 2020-age;
+		 System.out.println("You are most " + " likely to be born in "
+				 +  birthYear);
+		 
+	 }
+}
+	 
