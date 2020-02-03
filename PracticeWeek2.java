7be21453f466c73f31df6fdf08a1e75665215125
Wilfyortega26 <60441221+Wilfyortega26@users.noreply.github.com>
2020-02-03 12:42:15 -0500

    Update PracticeWeek2.java

--- PracticeWeek2.java
+++ PracticeWeek2.java
@@ -1,4 +1,23 @@
-
-public class PracticeWeek2 {
+/**
+ * @author Wilfy Ortega
+ * created    :2/03/20
+ * description: variable practice
+ **/
+  public class PracticeWeek2 {
+	  public static void main(String[] args) {
+	  // Define variables
+	     double wallet = 10.35;
+	     byte cash     = 5;
+	     int number    = 3;
+	     
+      // Declaring variables
+          String name1;
+          String name2;
+          String name3;
+          
+      // Assign values
+          name1 ="Cefy";
+          name2 ="Dog";
+          name3 ="Bax";
 
 }
