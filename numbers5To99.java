3355f4a6c57821b04be85457963e5c9222bbee11
Wilfyortega26 <60441221+Wilfyortega26@users.noreply.github.com>
2020-02-26 12:41:09 -0500

    Add files via upload

---
+++ numbers5To99.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Wilfy Ortega
+ *
+ */
+
+public class numbers5To99 {
+ public static void main(String[] args) {
+	
+	 int i = 0;
+	 int num = 0;
+	 
+	 for (i= 5; i <= 99;  i++) {
+	 String Form = String.format("%02d",i);
+	 
+	 System.out.println(Form);
+	 
+	 
+	 } 
+ }
+}
